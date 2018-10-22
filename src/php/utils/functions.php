ecc8473fb6e85a02465b7073110004b75fe8a0bf
Jan Henzi <jan.henzi@hotmail.com>
2018-10-22 19:20:46 +0200

    init product array stuff and gitignore file

--- src/php/utils/functions.php
+++ src/php/utils/functions.php
@@ -1,20 +1,37 @@
 <?php
-
 function getPageContent($name) {
     switch ((isset($_GET[$name]) ? $_GET[$name] : '')) {
         case 'rods':
             echo "<p> rods </p>";
             break;
         default:
-            echo "<p> default </p>";
+            $products = getProducts();
+            foreach ($products as $key => $product) {
+              echo "<div>
+                <p>$key</p>
+                <p>$product[0]</p>
+                <p>$product[1]</p>
+                </div>";
+            }
             break;
     }
 }
-
 function displayNav($pages) {
     echo "<nav><ul>";
     foreach ($pages as $page) {
         echo "<li class=\"$page[0]\"><a href=\"index.php?page=$page[1]\" alt=\"$page[1]\">$page[2]</a></li>";
     }
     echo "</nav></ul>";
+}
+
+function getProducts() {
+    $lines = file("content/products.txt");
+    $products = array();
+    foreach ($lines as $line) {
+        $product = explode(";", $line);
+        $key = $product[0];
+        $value = array($product[1], $product[2]);
+        $products[$key] = $value;
+    }
+    return $products;
 }
