e973b095e7c6f901dbbdbcfcd1ecdfdceb99ff40
Jan Henzi <jan.henzi@hotmail.com>
2018-10-22 17:42:43 +0200

    switch case for content load and some functions

--- src/index.php
+++ src/index.php
@@ -1,36 +1,24 @@
 <?php
     include 'php\utils\functions.php';
     $pages = array(
-        "rods" => array("nav-left", "rods.php", "Fishing Rods"),
-        "reels" => array("nav-left", "reels.php", "Reels")
+        "rods" => array("nav-left", "rods", "Fishing Rods"),
+        "reels" => array("nav-left", "reels", "Reels"),
+        "lures" => array("nav-left", "lures","Lures"),
+        "lines" => array("nav-left", "lines","Fishing Lines"),
+        "accessories" => array("nav-left", "accessories","Accessories"),
+        "about" => array("nav-left", "about","About"),
+        "login" => array("nav-right", "login","Login"),
+        "#en" => array("nav-right", "#en","English"),
+        "#de" => array("nav-right", "#de","Deutsch")
     );
-require 'html/head.html';?>
+    require 'html/head.html';?>
 <body>
     <header>
     </header>
-    <?php
-    $nav = array(
-        array("nav-left", "rods", "Fishing Rods"),
-        array("nav-left", "reels","Reels"),
-        array("nav-left", "lures","Lures"),
-        array("nav-left", "lines","Fishing Lines"),
-        array("nav-left", "accessories","Accessories"),
-        array("nav-left", "about","About"),
-        array("nav-right", "login","Login"),
-        array("nav-right", "#en","English"),
-        array("nav-right", "#de","Deutsch")
-    );
-    echo "<nav><ul>";
-    foreach ($nav as $item) {
-        echo "<li class=\"$item[0]\"><a href=\"$item[1].html\" alt=\"$item[1]\">$item[2]</a></li>";
-    }
-    echo "</nav></ul>";
-    ?>
+    <?php displayNav($pages); ?>
     <div id="content">
-        <?php
-        ?>
-        <p>Hello, world!</p>
         <img src="img/logo.png" alt="Logo" id="logo">
+        <?php echo getPageContent("page"); ?>
     </div>
     <footer>Footer</footer>
     <script src="js/script.js"></script>
--- src/php/utils/functions.php
+++ src/php/utils/functions.php
@@ -1,8 +1,20 @@
 <?php
 
-function getPageName($name, $default) {
-    if (!isset($name)) {
-        return $default;
+function getPageContent($name) {
+    switch ((isset($_GET[$name]) ? $_GET[$name] : '')) {
+        case 'rods':
+            echo "<p> rods </p>";
+            break;
+        default:
+            echo "<p> default </p>";
+            break;
     }
-    return $name;
+}
+
+function displayNav($pages) {
+    echo "<nav><ul>";
+    foreach ($pages as $page) {
+        echo "<li class=\"$page[0]\"><a href=\"index.php?page=$page[1]\" alt=\"$page[1]\">$page[2]</a></li>";
+    }
+    echo "</nav></ul>";
 }
